6c7108c1701f1202e1475576a39cfe752555c899
jotamurua <102777812+jotamurua@users.noreply.github.com>
2022-05-11 21:08:59 -0300

    a lot!
    inyectamos api
    agregamos data a la interfaz

--- ang-demo-rest/src/app/app.module.ts
+++ ang-demo-rest/src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { PokemonComponent } from './components/pokemon/pokemon.component';
     PokemonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ang-demo-rest/src/app/components/pokemon/pokemon.component.ts
+++ ang-demo-rest/src/app/components/pokemon/pokemon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonComponent implements OnInit {
 
-  constructor() { }
+  name: string
+  urlImage: string
+  urlImageB: string
+  urlImageC: string
+  urlImageD: string
+  hp: string
+  attack: string
+  defense: string
+  specialattack: string
+  specialdefense: string
+  speed: string
+
+  constructor(private pokemonService : PokemonService) { }
+
 
   ngOnInit(): void {
   }
 
+  search() {
+    this.pokemonService.getPokemon(this.name).subscribe((data:any) => {
+      this.urlImage = data.sprites.front_shiny
+      this.urlImageB = data.sprites.back_shiny
+      this.urlImageC = data.sprites.front_shiny_female
+      this.urlImageD = data.sprites.back_shiny_female
+      this.hp = data.stats[0].base_stat
+      this.attack = data.stats[1].base_stat
+      this.defense = data.stats[2].base_stat
+      this.specialattack = data.stats[3].base_stat
+      this.specialdefense = data.stats[4].base_stat
+      this.speed = data.stats[5].base_stat
+    })
+  }
 }
